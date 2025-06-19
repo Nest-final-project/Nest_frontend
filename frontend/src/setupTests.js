61ec7944c52adbfc85d118c348408ae834564dff
suj9810 <suj9810@naver.com>
2025-06-20 02:03:16 +0900

    Migrate from CRA to Vite - Update build system and configuration

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -1,5 +1,17 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// global test setup
+global.ResizeObserver = global.ResizeObserver || class ResizeObserver {
+  constructor(cb) {
+    this.cb = cb;
+  }
+  observe() {
+    return null;
+  }
+  unobserve() {
+    return null;
+  }
+  disconnect() {
+    return null;
+  }
+};
